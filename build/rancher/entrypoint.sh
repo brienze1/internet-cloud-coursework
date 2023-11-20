4b39963d1583a617c8a9812086b885b52cbd907b
Luis Brienze <lfbrienze@gmail.com>
2023-11-20 18:35:52 +0000

    added kubernetes files to deploy whiteboard api, added localstack to deploy the DynamoDB instance, and fixed some issues with integrating the containers

--- build/rancher/entrypoint.sh
+++ build/rancher/entrypoint.sh
@@ -9,21 +9,20 @@ echo "########### Rancher 1 is up ###########"
 
 echo "########### Getting token for rancher 1 ###########"
 TOKEN=$(curl -k -s 'https://rancher1/v3-public/localProviders/local?action=login' -H 'content-type: application/json' --data-binary '{"username":"admin","password":"password","ttl":0}' | jq -r .token)
+sleep 2
 
 echo "########### Authenticating rancher 1 ###########"
 rancher login https://rancher1/v3 --token "$TOKEN" --skip-verify
+sleep 2
 
-echo "########### Deploying static-site on rancher 1 ###########"
-rancher kubectl create deployment static-site --image prakhar1989/static-site --server https://rancher1
+echo "########### Deploying whiteboard-api on rancher 1 ###########"
+rancher kubectl create -f /init-scripts/rancher-cli/eks/deployment.yml --server https://rancher1
 
-echo "########### Exposing static-site port 80 on rancher 1 ###########"
-rancher kubectl expose deployment static-site --type=NodePort --port=80 --name=static-site-service --server https://rancher1
+echo "########### Adding HPA to whiteboard-api on rancher 1 ###########"
+rancher kubectl create -f /init-scripts/rancher-cli/eks/hpa.yml --server https://rancher1
 
-echo "########### Patching NodePort to port 30000 on rancher 1 ###########"
-rancher kubectl patch service static-site-service --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]' --server https://rancher1
-
-echo "########### Updating static-site replica number to 2 on rancher 1 ###########"
-rancher kubectl scale deployment static-site --replicas=2 --server https://rancher1
+echo "########### Adding NodePort service to whiteboard-api on rancher 1 ###########"
+rancher kubectl create -f /init-scripts/rancher-cli/eks/service.yml --server https://rancher1
 
 # RANCHER 2
 echo "########### Waiting rancher 2 to become healthy ###########"
@@ -34,18 +33,17 @@ echo "########### Rancher 2 is up ###########"
 
 echo "########### Getting token for rancher 2 ###########"
 TOKEN=$(curl -k -s 'https://rancher2/v3-public/localProviders/local?action=login' -H 'content-type: application/json' --data-binary '{"username":"admin","password":"password","ttl":0}' | jq -r .token)
+sleep 2
 
 echo "########### Authenticating rancher 2 ###########"
 rancher login https://rancher2/v3 --token "$TOKEN" --skip-verify
+sleep 2
 
-echo "########### Deploying static-site on rancher 2 ###########"
-rancher kubectl create deployment static-site --image prakhar1989/static-site --server https://rancher2
+echo "########### Deploying whiteboard-api on rancher 2 ###########"
+rancher kubectl create -f /init-scripts/rancher-cli/eks/deployment.yml --server https://rancher2
 
-echo "########### Exposing static-site port 80 on rancher 2 ###########"
-rancher kubectl expose deployment static-site --type=NodePort --port=80 --name=static-site-service --server https://rancher2
+echo "########### Adding HPA to whiteboard-api on rancher 2 ###########"
+rancher kubectl create -f /init-scripts/rancher-cli/eks/hpa.yml --server https://rancher2
 
-echo "########### Patching NodePort to port 30000 on rancher 2 ###########"
-rancher kubectl patch service static-site-service --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]' --server https://rancher2
-
-echo "########### Updating static-site replica number to 2 on rancher 2 ###########"
-rancher kubectl scale deployment static-site --replicas=2 --server https://rancher2
+echo "########### Adding NodePort service to whiteboard-api on rancher 2 ###########"
+rancher kubectl create -f /init-scripts/rancher-cli/eks/service.yml --server https://rancher2
--- scripts/docker-up.sh
+++ scripts/docker-up.sh
@@ -2,6 +2,6 @@
 
 docker-compose -f build/docker-compose.yml down --volumes
 
-docker-compose -f build/docker-compose.yml build --no-cache
+#docker-compose -f build/docker-compose.yml build --no-cache
 
 docker-compose -f build/docker-compose.yml up
