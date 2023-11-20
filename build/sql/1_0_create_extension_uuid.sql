870f98355bb620fd947e628db3d74d9784bb8a78
Luis Brienze <lfbrienze@gmail.com>
2023-11-20 23:45:42 +0000

    removed unwanted files

--- build/sql/1_0_create_extension_uuid.sql
+++
@@ -1,4 +0,0 @@
-SET SCHEMA 'test_db';
-
-DROP EXTENSION IF EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" with schema test_db;
--- build/sql/2_0_create_table_world_stats.sql
+++
@@ -1,8 +0,0 @@
-SET SCHEMA 'test_db';
-
-CREATE TABLE test_table
-(
-    id   uuid        NOT NULL DEFAULT uuid_generate_v4(),
-    name varchar(20) NOT NULL,
-    CONSTRAINT id PRIMARY KEY (id)
-);
